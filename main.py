2b123a403dff61c3ddd5dddf39cae70b14c9817d
JeongminJeon <jmjeon3155@gmail.com>
2019-01-18 21:55:25 +0900

    delete face

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 face_cascade = cv2.CascadeClassifier("./haarcascade/haarcascade_frontalface_default.xml")
 
-
+def DrawCircle(img, rects, color):
+    for x, y, w, h in rects:
+        cv2.circle(img, (x+int(w/2),y+int(h/2)), int(h/2), (255,255,255), -1)
+        
+    
 while True:
     
     ret, frame = cap.read()
     
-    #카메라오류
     if not ret:
         break
 
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,1.08,5)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
     for x,y,w,h in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-    
-    cv2.imshow("Test",frame)
+        #cv2.circle(frame, (x+int(w/2),y+int(h/2)), int(h/2), (255,255,255), -1)
+        DrawCircle(frame, faces, (255,255,255))
+        
+        
+    cv2.imshow("Face",frame)
     if cv2.waitKey(1)>0:
         break
 
