b3892bc718c14d869bbd25167c9f386db5b42184
Jeongmin <jmjeon3155@gmail.com>
2019-01-25 12:00:44 +0900

    HSV손검출

--- main_video.py
+++ main_video.py
@@ -19,17 +19,49 @@ def maskedByYCrCb(img):
     masked = cv2.inRange(ycrcb,np.array([0,133,77]),np.array([255,173,127]))
     return masked
 
+#HSV색상을 이용하여 피부색검출
+def maskedByHSV(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_high = cv2.getTrackbarPos('H_high', 'color_HSV')
+    h_low = cv2.getTrackbarPos('H_low', 'color_HSV')
+    s_high = cv2.getTrackbarPos('S_high', 'color_HSV')
+    s_low = cv2.getTrackbarPos('S_low', 'color_HSV')
+    v_high = cv2.getTrackbarPos('V_high', 'color_HSV')
+    v_low = cv2.getTrackbarPos('V_low', 'color_HSV')
+
+    lower_thr = np.array([h_low, s_low, v_low])
+    upper_thr = np.array([h_high,s_high,v_high])
+
+    mask = cv2.inRange(hsv, lower_thr, upper_thr)
+    return mask
+
+
+
+
 #얼굴에 사각형 그리기
 def DrawRect(img, rects, color):
     for x,y,w,h in rects:
         cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
 
-def findHand(img):
-    ret, thr = cv2.threshold(img,127,255,0)
+def DrawContourHand(img,mask):
+    ret, thr = cv2.threshold(mask,127,255,0)
     contours, _ = cv2.findContours(thr, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img, contours, -1, (0,255,0),1)
 
-    
+def onChange(x):
+    pass
+
+def createTrackBarHSV():
+    cv2.namedWindow('color_HSV')
+    #트랙바 생성
+    cv2.createTrackbar('H_low', 'color_HSV', 5,179,onChange)
+    cv2.createTrackbar('H_high', 'color_HSV', 33,179,onChange)
+    cv2.createTrackbar('V_low', 'color_HSV', 64,255,onChange)
+    cv2.createTrackbar('V_high', 'color_HSV', 175,255,onChange)
+    cv2.createTrackbar('S_low', 'color_HSV', 122,255,onChange)
+    cv2.createTrackbar('S_high', 'color_HSV', 255,255,onChange)
+
+HSV_first_flag = True
 while True:
     
     ret, frame = cap.read()
@@ -45,15 +77,17 @@ while True:
     DrawRect(frame, faces, (0,255,0))
 
     #원본 영상에 살색검출
-    masked = maskedByYCrCb(frame)
-
+    if(HSV_first_flag):
+        createTrackBarHSV()
+        HSV_first_flag=False
+    masked = maskedByHSV(frame)    
+    #masked = maskedByYCrCb(frame)
+    
     #마스크 이미지에 얼굴 가리기
     DrawCircle(masked, faces, (255,255,255))
 
     #손 Contour그리기
-    #findhandContour(masked)
-
-
+    DrawContourHand(frame,masked)
         
     cv2.imshow("Face",frame)
     cv2.imshow("masked",masked)
